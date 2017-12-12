32505fb8d61f6ac4c0c8abba6e9eb8cf31c2535b
Mikoto Zhang <i@electromaster.me>
2017-12-12 01:45:13 -0600

    Translation Added
    Added Chinese Support.

--- app/src/main/java/me/electromaster/morsecode/EncodeMessageActivity.java
+++ app/src/main/java/me/electromaster/morsecode/EncodeMessageActivity.java
@@ -25,113 +25,112 @@ public class EncodeMessageActivity extends AppCompatActivity {
         String str2 = "";
         for (int i = 0; i < str.length; i++) {
             if (str[i] == 'a') {
-                str2+=".- ";
+                str2 += ".- ";
             }
             if(str[i] == 'b') {
-                str2+="-... ";
+                str2 += "-... ";
             }
             if(str[i] == 'c') {
-                str2+="-.-. ";
+                str2 += "-.-. ";
             }
             if(str[i] == 'd') {
-                str2+="-.. ";
+                str2 += "-.. ";
             }
             if(str[i] == 'e') {
-                str2+=". ";
+                str2 += ". ";
             }
             if(str[i] == 'f') {
-                str2+="..-. ";
+                str2 += "..-. ";
             }
             if(str[i] == 'g') {
-                str2+="--. ";
+                str2 += "--. ";
             }
             if(str[i] == 'h') {
-                str2+=".... ";
+                str2 += ".... ";
             }
             if(str[i] == 'i') {
-                str2+=".. ";
+                str2 += ".. ";
             }
             if(str[i] == 'j') {
-                str2+=".--- ";
+                str2 += ".--- ";
             }
-
             if (str[i] == 'k') {
-                str2+="-.- ";
+                str2 += "-.- ";
             }
             if(str[i] == 'l') {
-                str2+=".-.. ";
+                str2 += ".-.. ";
             }
             if(str[i] == 'm') {
-                str2+="-- ";
+                str2 += "-- ";
             }
             if(str[i] == 'n') {
-                str2+="-. ";
+                str2 += "-. ";
             }
             if(str[i] == 'o') {
-                str2+="--- ";
+                str2 += "--- ";
             }
             if(str[i] == 'p') {
-                str2+=".--. ";
+                str2 += ".--. ";
             }
             if(str[i] == 'q') {
-                str2+="--.- ";
+                str2 += "--.- ";
             }
             if(str[i] == 'r') {
-                str2+=".-. ";
+                str2 += ".-. ";
             }
             if(str[i] == 's') {
-                str2+="... ";
+                str2 += "... ";
             }
             if(str[i] == 't') {
-                str2+="- ";
+                str2 += "- ";
             }
             if (str[i] == 'u') {
-                str2+="..- ";
+                str2 += "..- ";
             }
             if(str[i] == 'v') {
-                str2+="...- ";
+                str2 += "...- ";
             }
             if(str[i] == 'w') {
-                str2+=".-- ";
+                str2 += ".-- ";
             }
             if(str[i] == 'x') {
-                str2+="-..- ";
+                str2 += "-..- ";
             }
             if(str[i] == 'y') {
-                str2+="-.-- ";
+                str2 += "-.-- ";
             }
             if(str[i] == 'z') {
-                str2+="--.. ";
+                str2 += "--.. ";
             }
             if(str[i] == '0') {
-                str2+="----- ";
+                str2 += "----- ";
             }
             if(str[i] == '1') {
-                str2+=".---- ";
+                str2 += ".---- ";
             }
             if(str[i] == '2') {
-                str2+="..--- ";
+                str2 += "..--- ";
             }
             if(str[i] == '3') {
-                str2+="...-- ";
+                str2 += "...-- ";
             }
             if(str[i] == '4') {
-                str2+="....- ";
+                str2 += "....- ";
             }
             if(str[i] == '5') {
-                str2+="..... ";
+                str2 += "..... ";
             }
             if(str[i] == '6') {
-                str2+="-.... ";
+                str2 += "-.... ";
             }
             if(str[i] == '7') {
-                str2+="--... ";
+                str2 += "--... ";
             }
             if(str[i] == '8') {
-                str2+="---.. ";
+                str2 += "---.. ";
             }
             if(str[i] == '9') {
-                str2+="----. ";
+                str2 += "----. ";
             }
         }
         return str2;
